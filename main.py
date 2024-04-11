6d925c4a74c4fa49a65c61a5aac00ee9009b8977
Nishant <toanishant@gmail.com>
2024-04-11 12:26:46 +0530

    Building initialization and basic functions

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-def show_main_menu():
-    print("1.Create new board\n2.View board")
-    x = input("Choose an option: ")
-    return x
+import mysql.connector as sqltor
+from prettytable import PrettyTable
+mycon = sqltor.connect(host ='localhost',user = 'root',passwd = 'root@123')
+cursor = mycon.cursor()
+def initialization():
+    cursor.execute("create database IF NOT EXISTS Kanban")
+    cursor.execute("use Kanban")
+    cursor.execute("CREATE TABLE IF NOT EXISTS Kanban_Table (Taskname VARCHAR(255),Status VARCHAR(50),Reportee VARCHAR(100),Assignee VARCHAR(100),Priority INT);")
+
+def isempty():
+    cursor.execute("select * from Kanban_Table;")
+    data = cursor.fetchall()
+    if(not data):
+        return 1
+    else:
+        return 0
+initialization()
+mycon.close()
+        
