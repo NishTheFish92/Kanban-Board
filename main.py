28af933ac69346e6ab5554f6b4ab6cbd7a3c5e5b
Nishant <toanishant@gmail.com>
2024-04-11 13:03:41 +0530

    Adding more functionalities

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ cursor = mycon.cursor()
 def initialization():
     cursor.execute("create database IF NOT EXISTS Kanban")
     cursor.execute("use Kanban")
-    cursor.execute("CREATE TABLE IF NOT EXISTS Kanban_Table (Taskname VARCHAR(255),Status VARCHAR(50),Reportee VARCHAR(100),Assignee VARCHAR(100),Priority INT);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS Kanban_Table (Taskname VARCHAR(255),Status VARCHAR(50),Priority INT,Reportee VARCHAR(100),Assignee VARCHAR(100));")
 
 def isempty():
     cursor.execute("select * from Kanban_Table;")
@@ -14,6 +14,20 @@ def isempty():
         return 1
     else:
         return 0
-initialization()
+def addrows(x):
+    for i in range(x):
+        taskname = input("Enter taskname: ")
+        status = input("Enter status (To-do,In-progress,Done): ")
+        priority = int(input("Enter priority rank (1 highest,3 lowest): "))
+        reportee = input("Enter reportee: ")
+        assignee = input("Enter assignee: ")
+        cursor.execute("INSERT INTO Kanban_Table (Taskname, Taskdesc, Status,Priority, Reportee, Assignee) VALUES ({},{},{},{},{})".format(taskname,status,priority,reportee,assignee))
+
+def displaykanban():
+    if isempty():
+        print("No tasks present,Please add some tasks.")
+        rows = int(input("How many tasks would you like to add?"))
+        addrows(rows)
+    else:
+        #Print entire Kanban table
 mycon.close()
-        
